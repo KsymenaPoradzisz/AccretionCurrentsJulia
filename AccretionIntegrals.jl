58cb85dbcf821bab4a3349bf425c8b77880c4c21
Ksymena Poradzisz <73689092+KsymenaPoradzisz@users.noreply.github.com>
2023-11-27 15:25:11 +0100

    Adding j_phi

--- AccretionIntegrals.jl
+++ AccretionIntegrals.jl
@@ -3,7 +3,7 @@ Author: Ksymena Poradzisz
 Contact: ksymena.poradzisz@gmail.com
 Affiliation: Jagiellonian University
 Created: [2023-10-21]
-Updated: [2023-11-18]
+Updated: [2023-11-27]
 Description:
 This Julia script is intended to compute integrals for black hole accretion currents J^\mu
 =#
@@ -13,7 +13,6 @@ using QuadGK
 using DoubleExponentialFormulas, CSV
 @variables M, m_0, ξ, ϵ_σ, ϵ_r, ε, α, λ, _ξ_,φ
 machine_epsilon = eps() #the smallest number which can be added to 1.0
-#X(ξ, ε, λ, α) = quadde(_ξ_-> (λ + (α*ε)/(1-2/_ξ_))/( ( (α^2)/(1-2/_ξ_) + _ξ_^2  ) * sqrt(ε^2 - (1-2/_ξ_)*(1+λ^2/_ξ_^2) - (α^2+2*ε*λ*α)/(_ξ_^2)) ), ξ, Inf, rtol = 1e-5)[1] 
 X(ξ, ε, λ, α) = quadde(_ξ_-> (λ + (α*ε)/(1-2/_ξ_))/( ( (α^2)/(1-2/_ξ_) + _ξ_^2  ) * sqrt(ε^2 - (1-2/_ξ_)*(1+λ^2/_ξ_^2) - (α^2+2*ε*λ*α)/(_ξ_^2)) ), ξ, Inf, rtol = 1e-5)[1] 
 
 v = 0.5 # predkosc w jedn. c
@@ -30,7 +29,6 @@ U_λ(ξ, λ) = (1-2/ξ)*(1+λ^2/ξ^2)
 R̃(ξ,ε,α,λ ) = ε^2 - U_λ(ξ,λ) - α * (α + 2*ε*λ*ϵ_σ)/ξ^2
 
 #Definicja S w https://www.actaphys.uj.edu.pl/fulltext?series=Sup&vol=16&aid=6-A13 poniżej wzorów (11). 
-#Definicja S w https://www.actaphys.uj.edu.pl/fulltext?series=Sup&vol=16&aid=6-A13 poniżej wzorów (11). 
 S(ξ, ε, λ, α, ϵ_σ, ϵ_r)  = exp(-(ε+ϵ_σ*v*sqrt(ε^2-1)*sin( (π/2 -X(ξ,ε, λ, α))*(φ-ϵ_σ*ϵ_r)))*β/sqrt(1-v^2) )
 
 #functions which are in integrals of J currents
@@ -43,6 +41,9 @@ function __jr_integrals__(ξ,  λ,ε, α, ϵ_σ, ϵ_r=-1)
     temp = ϵ_r * S(ξ, ε, λ, α, ϵ_σ, ϵ_r) 
     return temp
 end
+function __jφ_integrals__(ξ, λ,ε, α, ϵ_σ, ϵ_r = -1 )
+    temp = ϵ_σ * S(ξ, ε, λ, α, ϵ_σ, ϵ_r)*(λ + ϵ_σ * α * ε)/sqrt(R̃(ξ, ε,α, λ))
+end
 
 #Calculation of integrals in different J current components
 function jt_ABS_integrals(f, ksi, alfa, eps_sigma, eps_r)
@@ -51,7 +52,10 @@ function jt_ABS_integrals(f, ksi, alfa, eps_sigma, eps_r)
     return result
     
 end
-
+function jφ_ABS_integrals(f, ksi, alfa, eps_sigma, eps_r)
+    temp(λ,ε ) = f(ksi, λ, ε, alfa, eps_sigma, eps_r)
+    result_err = quadgk(ε-> quadgk(λ-> temp(λ, ε), 0, sqrt(12/(1- (4)/(3*ε)/sqrt(9*ε^2)+1)))[1], 1, Inf)
+end
 
 function jr_ABS_integrals(f,  ksi, alfa, eps_sigma, eps_r)
     temp(λ, ε) = f(ksi,  λ,ε, alfa, eps_sigma, eps_r)
@@ -68,8 +72,7 @@ for i in 1:1
     eps_sigma = 1
     eps_r = -1
     try
-        res = jr_integrals(__jr_integrals__,  ksi, alfa, eps_sigma, eps_r)
-        println(res)
+        print("test")
     catch e_rror
         println(e_rror)
     end
