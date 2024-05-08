13c81308b2f7e88bfc8e363e98dc701677051398
Ksymena Poradzisz <73689092+KsymenaPoradzisz@users.noreply.github.com>
2024-05-08 09:57:57 +0200

    Added results of all tests
    Corrected formula for S, code is working

--- AccretionIntegrals.jl
+++ AccretionIntegrals.jl
@@ -3,10 +3,29 @@ Author: Ksymena Poradzisz
 Contact: ksymena.poradzisz@gmail.com
 Affiliation: Jagiellonian University
 Created: [2023-10-21]
-Updated: [2024-04-20]
+Updated: [2024-05-07]
 Description:
 This Julia script is intended to compute integrals for black hole accretion currents J^\mu
 =#
+
+#=
+To run code:
+1. Update: in Linux shell:  juliaup update
+2. Install missing packages:
+	a) in Linux shell run julia
+	b) in Julia REPL (shell) run: 
+		import Pkg; Pkg.add("PolynomialRoots")
+		import Pkg; Pkg.add("Symbolics")
+		import Pkg; Pkg.add("QuadGK")
+		import Pkg; Pkg.add("Polynomials")
+3. Run code from Linux shell: julia AccretionIntegrals_20240507.jl 
+
+Expected outcome:
+
+ 
+
+=#
+
 using Symbolics
 using QuadGK
 using PolynomialRoots
@@ -20,6 +39,9 @@ v = 0.5 # predkosc w jedn. c
 ################FUNCTIONS############
 
 X(ξ, ε, λ, α) = quadgk(_ξ_-> (λ + (α*ε)/(1-2/_ξ_))/( ( (α^2)/(1-2/_ξ_) + _ξ_^2  ) * sqrt(Complex(ε^2 - (1-2/_ξ_)*(1+λ^2/_ξ_^2) - (α^2+2*ε*λ*α)/(_ξ_^2))) ), ξ, Inf, rtol = 1e-5)[1] 
+
+
+println("X=",X(11,2,1/2,1/111))
 
 #Definicja U_λ 
 U_λ(ξ, λ) = (1-2/ξ)*(1+λ^2/ξ^2)
@@ -38,7 +60,8 @@ function R̃(ξ,ε,α,λ,ϵ_σ )
 end
 
 #Definicja S w https://www.actaphys.uj.edu.pl/fulltext?series=Sup&vol=16&aid=6-A13 poniżej wzorów (11). 
-S(ξ, ε, λ, α, ϵ_σ, ϵ_r)  = exp(-(ε+ϵ_σ*v*sqrt(ε^2-1)*sin( (π/2 -X(ξ,ε, λ, α))*(φ-ϵ_σ*ϵ_r)))*β/sqrt(1-v^2) )
+#S(ξ, ε, λ, α, ϵ_σ, ϵ_r)  = exp(-(ε+ϵ_σ*v*sqrt(ε^2-1)*sin( (π/2 -X(ξ,ε, λ, α))*(φ-ϵ_σ*ϵ_r)))*β/sqrt(1-v^2) )
+S(ξ, ε, λ, α, ϵ_σ, ϵ_r)  = exp(-(ε+ϵ_σ*v*sqrt(ε^2-1)*sin(φ-ϵ_σ*ϵ_r*(π/2-X(ξ,ε, λ, α))))*β/sqrt(1-v^2) )
 
 function λ_max(ξ, ε, α,ϵ_σ )
     if ξ == 2 && sign(α)*ϵ_σ == 1
@@ -84,10 +107,16 @@ function __jt_integrals__(ξ, λ, ε, α, ϵ_σ, ϵ_r  = -1)
     if R̃(ξ, ε,α, λ,ϵ_σ) == 0
         return 0
     else
-        temp = ε*S(ξ, ε, λ, α, ϵ_σ, ϵ_r) /sqrt(R̃(ξ, ε,α, λ,ϵ_σ))  
+        # temp = ε*S(ξ, ε, λ, α, ϵ_σ, ϵ_r) /sqrt(R̃(ξ, ε,α, λ,ϵ_σ))  
+        temp = ε*S(ξ, ε, λ, α, ϵ_σ, ϵ_r) /sqrt(R̃(ξ, ε,α, λ,ϵ_σ))/(M*ξ) # Tu powinna być suma 2 składników z  ϵ_σ=+1 i ϵ_σ=-1
         return temp  
     end 
 end
+
+
+println("Jt_integrand=",__jt_integrals__(8, 1/2, 2, 1/111, 1, -1) )
+
+
 function __jr_integrals__(ξ,  λ,ε, α, ϵ_σ, ϵ_r=-1)
     if R̃(ξ, ε,α, λ,ϵ_σ) == 0
         return 0
@@ -127,13 +156,13 @@ function jr_ABS_integrals(f,  ksi, alfa, eps_sigma, eps_r)
 end
 function ξ_ph(eps_sigma, alfa)
     temp = 2 + 2*cos(2/3*acos(-eps_sigma*alfa))
-    println("ξ_ph = ", temp)
+   # println("ξ_ph = ", temp)
     return temp
 end
 function ξ_mb(eps_sigma, alfa)
   
     temp = 2 - eps_sigma*alfa + 2*sqrt(1-eps_sigma*alfa)
-    println("ξ_mb = ", temp)
+    #println("ξ_mb = ", temp)
     return temp 
 end
 
@@ -172,6 +201,7 @@ for i in 1:1
     alfa = 0.0001
     eps_sigma = 1
     eps_r = -1
+    println("ksi = ", ksi)
     try
     #print("lambda_c = ", λ_c(Float64(alfa), 2., Float64(eps_sigma), Float64(ksi)))
     test_phi_abs = jφ_ABS_integrals(__jφ_integrals__, ksi, alfa, eps_sigma, eps_r)
@@ -179,12 +209,16 @@ for i in 1:1
     test_r_abs = jr_ABS_integrals(__jr_integrals__,  ksi, alfa, eps_sigma, eps_r)
     ###########################################################################
     test_phi_scatt  = jφ_SCATT_integrals(__jφ_integrals__, ksi, alfa, eps_sigma, eps_r)
+    test_t_scatt = jt_SCATT_integrals(__jt_integrals__, ksi, alfa, eps_sigma, eps_r)
+    test_r_scatt = jr_SCATT_integrals(__jr_integrals__, ksi, alfa, eps_sigma, eps_r)
     ########################################################################
     println("Phi ABS = ",test_phi_abs )
     println("t ABS = ", test_t_abs)
     println("r ABS = ", test_r_abs)
     ##############################################
     println("phi SCATT = ", test_phi_scatt)
+    println("t SCATT = ", test_t_scatt)
+    println("r SCATT = ", test_r_scatt)
     
         #test_t = jt_SCATT_integrals(__jt_integrals__, ksi, alfa, eps_sigma, eps_r)
        # println("Time SCATT = ", test_t)
