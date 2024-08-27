ff2a922799c1e49e24621cb8e2db2ca6beb90205
VA00 <aodrzywolek@gmail.com>
2024-08-27 15:11:16 +0200

    Test repo

--- X_function.jl
+++ X_function.jl
@@ -1,18 +1,25 @@
+# Define a type alias to toggle precision
+const MyFloat = BigFloat  # Use BigFloat for high precision
+#const MyFloat = Float64  # Use Float64 for machine precision
+
+
 using Symbolics, Dates
 using DoubleExponentialFormulas, CSV
 @variables M, m_0, ξ, ϵ_σ, ϵ_r, R̃, ε, U_λ, α,  β,v, λ, _ξ_, λ_c,φ
 
-# Set global precision to 346 bits
-setprecision(BigFloat, 426) # it corresponds to mathematica working precision 128
-qde = QuadDE(BigFloat; h0=one(BigFloat)/8, maxlevel=10)
+# Check if MyFloat is BigFloat and set precision accordingly
+if MyFloat === BigFloat
+    setprecision(MyFloat, 426)  # Set precision to 426 bits (128 decimal digits) for BigFloat
+end
+qde = QuadDE(MyFloat; h0=one(MyFloat)/8, maxlevel=12)
 # Define the function X with high precision
 X_and_Error(ξ, ε, λ, α) = qde(_ξ_ -> 
-(BigFloat(λ) + (BigFloat(α) * BigFloat(ε)) / (BigFloat(1) - BigFloat(2) / BigFloat(_ξ_))) / 
-(((BigFloat(α)^2) / (BigFloat(1) - BigFloat(2) / BigFloat(_ξ_)) + BigFloat(_ξ_)^2) * 
-sqrt(BigFloat(ε)^2 - (BigFloat(1) - BigFloat(2) / BigFloat(_ξ_)) * 
-(BigFloat(1) + BigFloat(λ)^2 / BigFloat(_ξ_)^2) - 
-(BigFloat(α)^2 + BigFloat(2) * BigFloat(ε) * BigFloat(λ) * BigFloat(α)) / (BigFloat(_ξ_)^2))),
-BigFloat(ξ), BigFloat(Inf); rtol = 1e-64
+(MyFloat(λ) + (MyFloat(α) * MyFloat(ε)) / (MyFloat(1) - MyFloat(2) / MyFloat(_ξ_))) / 
+(((MyFloat(α)^2) / (MyFloat(1) - MyFloat(2) / MyFloat(_ξ_)) + MyFloat(_ξ_)^2) * 
+sqrt(MyFloat(ε)^2 - (MyFloat(1) - MyFloat(2) / MyFloat(_ξ_)) * 
+(MyFloat(1) + MyFloat(λ)^2 / MyFloat(_ξ_)^2) - 
+(MyFloat(α)^2 + MyFloat(2) * MyFloat(ε) * MyFloat(λ) * MyFloat(α)) / (MyFloat(_ξ_)^2))),
+MyFloat(ξ), MyFloat(Inf); rtol = 1e-64
 )
 
 
@@ -23,11 +30,11 @@ data = []
 # Run calculations
 for i in 1:1000
     # Generate random values with high precision
-    ksi = abs(BigFloat(rand(9:100)))
-    eps = abs(BigFloat(rand(2:20)))
-    lambda = abs(BigFloat(rand(1:20)))
-    alfa = abs(rand(BigFloat))
-    #println(i, " and ", ksi)
+    ksi = abs(MyFloat(rand(9:100)))
+    eps = abs(MyFloat(rand(2:20)))
+    lambda = abs(MyFloat(rand(1:20)))
+    #alfa = abs(rand(MyFloat))
+    alfa = MyFloat(rand(-15:15))/MyFloat(16)
 
     temp = []
     push!(temp, ksi)
@@ -37,8 +44,7 @@ for i in 1:1000
 
     try
         # Calculate the result and the elapsed time
-        t = @elapsed X_and_Error(ksi, eps, lambda, 0)
-        result,error = X_and_Error(ksi, eps, lambda, 0)
+        t = @elapsed ((result, error) = X_and_Error(ksi, eps, lambda, alfa))
 
 
         if isinf(result)
@@ -47,7 +53,6 @@ for i in 1:1000
             # Append results to the temp array
             push!(temp, result)
             push!(temp, t)
-            #push!(temp, error)
             push!(data, Tuple(temp))
             i +=1
         end
@@ -63,4 +68,4 @@ end
 # Display the data
 #println(data)
 timestamp_for_file = Dates.format(now(), "yyyy-mm-dd_HH-MM-SS")
-CSV.write("/home/korizekori/magisterka/Julia_DATA_$(timestamp_for_file).csv", data, header = ["ξ","ε","λ", "α", "Result (Julia)", "Elapsed time in Julia[s]"])
+CSV.write("Julia_DATA_$(timestamp_for_file).csv", data, header = ["ξ","ε","λ", "α", "Result (Julia)", "Elapsed time in Julia[s]"], encoding="UTF-8")
