6d89cb2022fd8cd6cb9ebe36634da7055f7ff7ba
VA00 <aodrzywolek@gmail.com>
2024-08-27 18:12:20 +0200

    Removed old obsolete lines from code

--- X_function.jl
+++ X_function.jl
@@ -3,15 +3,16 @@ const MyFloat = BigFloat  # Use BigFloat for high precision
 #const MyFloat = Float64  # Use Float64 for machine precision
 
 
-using Symbolics, Dates
+using Dates
 using DoubleExponentialFormulas, CSV
-@variables M, m_0, ξ, ϵ_σ, ϵ_r, R̃, ε, U_λ, α,  β,v, λ, _ξ_, λ_c,φ
+
 
 # Check if MyFloat is BigFloat and set precision accordingly
 if MyFloat === BigFloat
     setprecision(MyFloat, 426)  # Set precision to 426 bits (128 decimal digits) for BigFloat
 end
-qde = QuadDE(MyFloat; h0=one(MyFloat)/8, maxlevel=12)
+
+qde = QuadDE(MyFloat; h0=one(MyFloat)/8, maxlevel=10)
 # Define the function X with high precision
 X_and_Error(ξ, ε, λ, α) = qde(_ξ_ -> 
 (MyFloat(λ) + (MyFloat(α) * MyFloat(ε)) / (MyFloat(1) - MyFloat(2) / MyFloat(_ξ_))) / 
@@ -28,18 +29,16 @@ MyFloat(ξ), MyFloat(Inf); rtol = 1e-64
 data = []
 
 # Run calculations
-for i in 1:1000
+for i in 1:10000
     # Generate random values with high precision
-    ksi = abs(MyFloat(rand(9:100)))
-    eps = abs(MyFloat(rand(2:20)))
-    lambda = abs(MyFloat(rand(1:20)))
-    #alfa = abs(rand(MyFloat))
-    alfa = MyFloat(rand(-15:15))/MyFloat(16)
+    ksi    = MyFloat(rand(9:100))
+    eps    = MyFloat(rand(2:20))
+    lambda = MyFloat(rand(1:20))
+    alfa   = MyFloat(rand(-15:15))/MyFloat(16)
 
     temp = []
     push!(temp, ksi)
     push!(temp, eps)
-    push!(temp, lambda)
     push!(temp, alfa)  # Initial value for alpha
 
     try
@@ -54,7 +53,6 @@ for i in 1:1000
             push!(temp, result)
             push!(temp, t)
             push!(data, Tuple(temp))
-            i +=1
         end
 
         
