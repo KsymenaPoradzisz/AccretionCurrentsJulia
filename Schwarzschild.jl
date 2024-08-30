d9b59bf4c7ca37dae888611b78f1dd39ff74c76d
Ksymena Poradzisz <73689092+KsymenaPoradzisz@users.noreply.github.com>
2024-08-30 11:43:59 +0200

    deleted unnecessary part of path

--- Schwarzschild.jl
+++ Schwarzschild.jl
@@ -100,7 +100,7 @@ data = DataFrame(r = r_values,φ = φ_values,x = x_values, y = y_values,timestam
                 J_X_TOTAlsch = J_X_TOTAL_sch_values, J_Y_TOTALsch = J_Y_TOTAL_sch_values, n = n_values)
 #saving data to a file
 timestamp_for_file = Dates.format(Dates.now(), "yyyy-mm-dd_HH-MM-SS")
-filename = "/home/korizekori/magisterka/Schwarzschild/data_Schwarzschild_$(timestamp_for_file).csv"
+filename = "data_Schwarzschild_$(timestamp_for_file).csv"
 if isfile(filename)
     CSV.write(filename, data, append = true)
 else
