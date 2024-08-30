360c4277d96a39a5482b686edddc0bdc0b3437e6
Ksymena Poradzisz <73689092+KsymenaPoradzisz@users.noreply.github.com>
2024-08-30 11:42:37 +0200

    Included file containing all necessary functions

--- Schwarzschild.jl
+++ Schwarzschild.jl
@@ -3,7 +3,7 @@ Author: Ksymena Poradzisz
 Contact: ksymena.poradzisz@gmail.com
 Affiliation: Jagiellonian University
 Created: [2024-06-28]
-Updated: [2024-08-26]
+Updated: [2024-08-30]
 Description:
 This Julia script is intended to compute integrals for Schwarzschild black hole accretion currents J^\mu and save them to *.csv file
 =#
@@ -15,6 +15,7 @@ using PolynomialRoots
 using Polynomials
 using Dates, CSV
 using DataFrames
+include("LIBSchw")
 
 const v = -1/2 # velocity in c units
 const β = 2  # Thermodynamic β=1/(kT), aka coolness
@@ -22,79 +23,6 @@ const γ = 1 / sqrt(1 - v^2) #global usage unnecessary
 const infinity = 150
 M = 1; m_0 = 1;
 
-#######SCHWARZSCHILD#############
-λ_max_sch(ξ, ε) = ξ * sqrt(ε^2 / (1 - 2 / ξ) - 1)
-λ_c_sch(ε) = sqrt(12 / (1 - 4 / (1 + 3 * ε / sqrt(9 * ε^2 - 8))^2))
-U_λ_sch(ξ, λ) = (1 - 2 / ξ) * (1 + λ^2 / ξ^2)
-function R_sch(ξ, ε, λ)
-    temp = ε^2 - U_λ_sch(ξ, λ)
-    if temp <= 0
-        return Inf
-    else
-        return temp
-    end
-
-end
-
-X_sch(ξ, ε, λ) = quadde(_ξ_-> λ/(_ξ_^2*sqrt(R_sch(_ξ_, ε, λ))),ξ,Inf)[1]
-
-function ε_min_sch(ξ)
-    if ξ<= 3
-        temp =  Inf
-    elseif ξ >= 4
-        temp = 1
-    elseif ξ <4 && ξ >3
-        temp = sqrt((1-2/ξ)*(1+1/(ξ-3)))
-    else
-        temp=  nothing
-    end
-
-    return temp
-end
-
-function J_t_ABS_sch(ξ,φ)
-    coeff = -2*m_0^3/ξ
-  #  coeff = 1
-    integral = coeff *  quadgk( ε-> ε* quadgk(λ-> cosh(β*γ*v*sqrt(ε^2-1)*sin(φ)*sin(X_sch(ξ, ε, λ)))*
-    exp(-β*γ*(ε+v*sqrt(ε^2-1)*cos(φ)*cos(X_sch(ξ,ε,λ))))/sqrt(R_sch(ξ, ε, λ)),0,λ_c_sch(ε))[1],1,infinity)[1]
-    return integral
-end
-function J_t_SCATT_sch(ξ, φ)
-    coeff = -4*m_0^3/ξ
-    #coeff = 1
-    integral=  coeff*quadgk( 
-                ε-> exp(-β*γ*ε) * ε *
-                 quadgk( λ ->  1*cosh(β*γ*v*sqrt(ε^2-1)*cos(φ)*cos(X_sch(ξ, ε, λ))) *cosh(β*γ*v*sqrt(ε^2-1)*sin(φ)*sin(X_sch(ξ, ε, λ)))/sqrt(R_sch(ξ, ε, λ)), λ_c_sch(ε), λ_max_sch(ξ,ε))[1],ε_min_sch(ξ), infinity)[1]
-    return integral
-end
-function J_r_ABS_sch(ξ,φ)
-    coeff = -2*m_0^3/(ξ-2) 
-    #coeff = 1
-    integral = coeff *quadgk( ε->  
-    quadgk(λ-> cosh(β*γ*v*sqrt(ε^2-1)*sin(φ)*sin(X_sch(ξ, ε, λ)))*exp(-β*γ*(ε+v*sqrt(ε^2-1)*cos(φ)*cos(X_sch(ξ,ε,λ)))),0,λ_c_sch(ε))[1],1,infinity)[1]
-    return integral
-end
-function J_r_SCATT_sch(ξ,φ)
-    coeff = 4*m_0^3/(ξ-2)
-    integral = coeff * quadgk(ε-> exp(-β*γ*ε) * quadgk(λ-> cosh(β*γ*v*sqrt(ε^2-1)*sin(φ)*sin(X_sch(ξ,ε,λ)))*sinh(β*γ*v*sqrt(ε^2-1)*cos(φ)*cos(X_sch(ξ,ε,λ))),λ_c_sch(ε),λ_max_sch(ξ,ε))[1],ε_min_sch(ξ), infinity)[1]
-    return integral
-end
-
-function J_φ_ABS_sch(ξ, φ)
-    coeff = -2 *M* m_0^3*M / ξ 
-   # coeff = 1
-    integral = coeff *quadgk(
-        ε -> quadgk(
-         λ-> λ/sqrt(R_sch(ξ, ε, λ)) * exp(-β*γ*(ε+v*sqrt(ε^2-1)*cos(φ)*cos(X_sch(ξ,ε,λ))))* 
-         sinh(β*γ*v*sqrt(ε^2-1)*sin(φ)*sin(X_sch(ξ,ε,λ))),0, λ_c_sch(ε))[1],1,infinity)[1]
-    return integral
-end
-function J_φ_SCATT_sch(ξ, φ)
-    coeff = -4 * M * m_0^3*M / ξ
-   # coeff = 1
-    integral = coeff * quadgk(ε-> exp(-β*γ*ε) *  quadgk(λ-> λ* sinh(β*γ*v*sqrt(ε^2-1)*sin(φ)*sin(X_sch(ξ,ε,λ))) * cosh(β*γ*v*sqrt(ε^2-1)*cos(φ)*cos(X_sch(ξ,ε,λ))) / sqrt(R_sch(ξ, ε, λ)),λ_c_sch(ε),λ_max_sch(ξ,ε))[1], ε_min_sch(ξ), infinity)[1]
-    return integral
-end
 
 
 function create_r_tbl(start, ending, step, M)
